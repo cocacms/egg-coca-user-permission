dc6ac208fca790c8b414fad753cd350b3787793b
rojer <rojerchen@qq.com>
2019-12-19 19:13:47 +0800

    adjust permission

--- app/middleware/permission.js
+++ app/middleware/permission.js
@@ -1,15 +1,19 @@
-'use strict';
+"use strict";
 
 module.exports = actionName => {
   return async function permission(ctx, next) {
+    const permissions = ctx.app.config.permission;
     const can =
-      [ ...ctx.logined.roles ].filter(x => {
+      [...ctx.logined.roles].filter(x => {
         return Array.isArray(x.permission) && x.permission.includes(actionName);
       }).length > 0;
 
     // 超级账号，无视任何权限
     if (!ctx.logined.superadmin && !can) {
-      ctx.throw(403, `您的账号类型没有操作此权限 [action: ${actionName}]`);
+      ctx.throw(
+        403,
+        `您的账号没有【${permissions[actionName] || actionName}】的操作权限`
+      );
     }
 
     await next();
