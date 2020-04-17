e8e80a095f5e02df698b1dc5136f3efb0c2a89fe
rojer <rojerchen@qq.com>
2020-04-17 16:48:59 +0800

    use right token prefix

--- app/middleware/user.js
+++ app/middleware/user.js
@@ -1,35 +1,35 @@
-'use strict';
+"use strict";
 module.exports = () => {
   return async function user(ctx, next) {
     let userId = null;
 
     const config = ctx.app.config.userPermimission;
-    if (config.dirver === 'session') {
+    if (config.dirver === "session") {
       userId = ctx.session.loginedId;
     }
 
-    if (config.dirver === 'jwt') {
-      const Authorization = ctx.get('Authorization');
+    if (config.dirver === "jwt") {
+      const Authorization = ctx.get("Authorization");
       if (Authorization) {
         try {
-          ctx.service.user.verify(Authorization.replace('Basic ', ''));
+          ctx.service.user.verify(Authorization.replace("Bearer ", ""));
         } catch (error) {
-          ctx.throw(401, '验证失败/过期，请重新登录');
+          ctx.throw(401, "验证失败/过期，请重新登录");
         }
         const payload = ctx.service.user.decode(
-          Authorization.replace('Basic ', '')
+          Authorization.replace("Bearer ", "")
         );
         userId = payload.id;
       }
     }
 
     if (!userId) {
-      ctx.throw(401, '请登录');
+      ctx.throw(401, "请登录");
     }
 
     ctx.logined = await ctx.app.model.User.findOne({
       where: { id: userId },
-      include: [ 'roles' ],
+      include: ["roles"],
     });
 
     await next();
